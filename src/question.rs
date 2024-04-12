97686d65ab144be70bae681837f9718f1edecfb4
Tejas013 <tejasmadhukar6@gmail.com>
2024-04-13 01:10:31 +0530

    Doing type mastrubation in rust will use this is future cli

--- src/question.rs
+++ src/question.rs
@@ -2,6 +2,15 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+// NOTE: for future use
 pub struct Question {
     question_input: io::Result<io::Lines<io::BufReader<File>>>,
 }
